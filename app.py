76d921a0112f061da3cda84f637246c594517663
lorena-fer <lorenavelazquezcorrea17@gmail.com>
2026-04-21 11:29:00 -0600

    app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def delete_device(id):
     return jsonify({"error": "No encontrado"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000, debug=True)
